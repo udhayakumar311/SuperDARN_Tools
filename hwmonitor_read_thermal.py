39c5781b40d7086d088f71e8112dee61c34ca0c7
MGuski <mguski@alaska.edu>
2016-12-22 17:55:22 -0900

    add realtime website overview and parsing of thermal hardware monitor data

---
+++ hwmonitor_read_thermal.py
@@ -0,0 +1,85 @@
+
+# %%
+import os
+import glob 
+
+
+
+# %%
+hwmonPath = "/sys/class/hwmon/"
+allDirs = os.listdir(hwmonPath)
+
+
+for currDir in allDirs: 
+    
+    nameFile = os.path.join(hwmonPath, currDir, "name")
+    if os.path.exists(nameFile):
+        with open(nameFile) as f:
+            hwmonName = f.read()[:-1]
+    else:
+        hwmonName = ""
+    
+    allTempFiles = glob.glob(os.path.join(hwmonPath, currDir, "temp*_input"))
+    
+    for iTemp, currTempFile in enumerate(allTempFiles):
+        with open(currTempFile) as f:
+                temp = float(f.read())/1000
+        
+        # read critical temperature        
+        critFileName =  currTempFile[:-5] + "crit"
+        if os.path.exists(critFileName):
+            with open(critFileName) as f:
+                tempCrit = float(f.read())/1000
+            if temp < tempCrit:
+                status = "ok (< {} C)".format(tempCrit)
+            else:
+                status = "critical! (> {} C)".format(tempCrit)
+        else:
+            status = "unknown"
+            
+        # read sensor name     
+        sensorFileName =  currTempFile[:-5] + "label"
+        if os.path.exists(sensorFileName):
+            with open(sensorFileName) as f:
+                sensorName = f.read()[:-1]
+        else:
+            sensorName = "sensor " + str(iTemp+1)      
+            
+        print("{}: {: <8} {: >15} = {: >5} C,  status: {}".format(currDir,hwmonName, sensorName, temp , status))
+
+
+
+
+#-%%
+#thermalPath = "/sys/devices/virtual/thermal/"
+thermalPath = "/sys/class/thermal/"
+allDirs = os.listdir(thermalPath)
+
+allDirs = [iDir for iDir in allDirs if iDir.startswith("thermal_zone")]
+
+allTemps = []
+allStatus = []
+for iDir in allDirs:
+    with open(os.path.join(thermalPath, iDir, "temp")) as f:
+        temp = float(f.read())/1000
+        allTemps.append(temp)    
+    iTripPoint = 0    
+    while (True):
+        tripFileName = os.path.join(thermalPath, iDir, "trip_point_{}_temp".format(iTripPoint))
+        if os.path.exists(tripFileName):
+            with open(tripFileName) as f:
+                tripTemp = float(f.read())/1000
+            if tripTemp > temp:
+                iTripPoint += 1
+            else: # found status
+                with open(os.path.join(thermalPath, iDir, "trip_point_{}_type".format(iTripPoint))) as f:
+                    allStatus.append(f.read()[:-1])
+                break
+        else:
+            allStatus.append("unknown")
+            break
+        
+
+
+for iDir, currDir in enumerate(allDirs):
+    print("{} : {} C Status: {}".format(currDir, allTemps[iDir], allStatus[iDir]))
