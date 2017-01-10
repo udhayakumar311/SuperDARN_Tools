8d3b8dbcf3d19b2bd81e19fb35ceb65185954830
MGuski <mguski@alaska.edu>
2017-01-09 20:52:48 -0900

    add simple menu and add realtime overview

---
+++ read_agent_log.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 20:08:27 2017
+
+@author: mguski
+
+- connect via remote_command()
+- read line from last position to end
+- zip / tar / whatever
+- transfer direct over std
+- unzip
+- parse text
+- create html contend
+
+"tail -n +15 monitor_test__2017-14-09.log | gzip -cf"
+
+"""
+
+
+
+
+
