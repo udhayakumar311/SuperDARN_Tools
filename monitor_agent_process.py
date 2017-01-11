97d896b07147d9d22665655e7cde5876ea593714
mgu <Martin.Guski@gmail.com>
2017-01-10 19:58:37 -0900

    small changes: automatic log file name

--- monitor_agent_process.py
+++ monitor_agent_process.py
@@ -19,34 +19,28 @@ f = open(fileName, 'r')
 
 re4section = re.compile("\[(.*)\]")
 configDict = dict()
+with open(fileName, 'r') as f:
+    for line in f:
+       if line.endswith("\n"):
+            line = line[:-1]
+       line = line.strip()
 
-for line in f:
-    if line.endswith("\n"):
-        line = line[:-1]
-    line = line.strip()
-
-    if line == "" or  line.startswith("#"):
-        continue
+       if line == "" or  line.startswith("#"):
+           continue
     
-    if re4section.match(line):
-        sectionName = re4section.search(line).group(1)
-        configDict[sectionName] = dict()
-        continue
+       if re4section.match(line):
+           sectionName = re4section.search(line).group(1)
+           configDict[sectionName] = dict()
+           continue
     
-    key, value = line.split(" ", 2)
-    configDict[sectionName][key] = value
+       key, value = line.split(" ", 2)
+       configDict[sectionName][key] = value
     
     
 # %%      
 
-startTime = datetime.datetime.now()
-logFile = configDict["general"]['logFile']
-logFile = logFile[:-4] +  startTime.strftime("__%Y-%m-%d") + logFile[-4:]
-with open(logFile, 'w+') as f:
-    f.write("Starting monitor agent on " + configDict["general"]['name'] + " (" + str(startTime) + ")\n\n")
     
-    
-
+logFile = None
 monitorList = []
 if "hdd_monitor" in configDict.keys():
     monitorList.append(hdd_monitor.hddSpaceMonitor(float(configDict["hdd_monitor"]['checkPeriod']), ))
@@ -56,7 +50,14 @@ if "computer_temperature" in configDict.keys():
 
   
 while True:
+    # create new log file
+    if logFile == None or datetime.datetime.now().day != startTime.day:
+        startTime = datetime.datetime.now()
+        logFile = "rom_" + configDict["general"]["name"] +  startTime.strftime("__%Y-%m-%d") + ".log"
+        with open(logFile, 'w+') as f:
+            f.write("Starting monitor agent on " + configDict["general"]['name'] + " (" + str(startTime) + ")\n\n")
     
+
     for mon in monitorList:
         if mon.minutes_to_next_check() <= 0:
             logMSG = mon.run()
