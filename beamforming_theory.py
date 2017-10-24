cae80c29f70679ecab876ffec56fe247c0295a11
MGuski <mguski@alaska.edu>
2017-10-23 20:10:09 -0800

    commit old changes...

--- beamforming_theory.py
+++ beamforming_theory.py
@@ -10,12 +10,36 @@ import matplotlib.pyplot as plt
 
 nAntennas = 16
 x_spacing = 15.4
-freq = 10e6
+freq = 8.1e6
 
 nSteps = 360*5
 
 c = 3e8
 alphaVec = np.array(range(nSteps)) /nSteps*2* np.pi
+# %%
+import numpy as np
+
+nBeams = 16
+x_spacing = 15.4
+beam_sep = 3.24
+c = 3e8
+
+center_beam = (nBeams -1)/2
+delay_list = []
+
+for iBeam in range(nBeams):
+ azm = (iBeam - center_beam) * beam_sep /180 * np.pi
+ delta_time = np.sin(azm) * x_spacing / c
+ delay_list.append(delta_time*1e9)
+ 
+
+print("Theory Values for:")
+print("  {} beams, {} deg beam sep, {} m antenna spacing".format(nBeams, beam_sep,x_spacing))
+
+for iBeam in range(nBeams):
+    print("  beam {: >3d} / {} :   time diff {: >7.3f} ns".format(iBeam, nBeams, delay_list[iBeam]))
+    
+
 
 
 # %% how to map anteeas to PCs, if one PC fails
